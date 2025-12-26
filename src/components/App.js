80f1419d20c705cfea90d5c381020ac9c480d4ce
Ishika Verma <ishikaverma2317@gmail.com>
2025-12-26 13:48:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,52 @@
+import React, { useState } from "react";
+import "../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
-const App = () => {
+  const addTodo = () => {
+    if (input.trim() === "") return;
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app">
+      <h1>To-Do List</h1>
+
+      <input
+        type="text"
+        placeholder="Enter todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        data-testid="todo-input"
+      />
+
+      <button onClick={addTodo} data-testid="add-todo">
+        Add Todo
+      </button>
+
+      <ul data-testid="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button
+              onClick={() => deleteTodo(index)}
+              data-testid={`delete-${index}`}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
